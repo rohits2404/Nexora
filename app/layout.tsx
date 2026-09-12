18b4b6b7ea314a500f3b2e05299092f6e4c396c8
Rohit <rohits8731@gmail.com>
2026-09-12 12:12:53 +0530

    02: Auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { ClerkProvider } from "@clerk/nextjs";
+import { dark } from "@clerk/ui/themes";
 
 const inter = Inter({
     subsets: ["latin"],
@@ -15,7 +17,11 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps<"/">) {
     return (
         <html lang="en">
-            <body className={inter.className}>{children}</body>
+            <body className={inter.className}>
+                <ClerkProvider appearance={{ theme: dark }}>
+                    {children}
+                </ClerkProvider>
+            </body>
         </html>
     );
 }
